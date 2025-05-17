fad1f7574d16200d108b479b5eba210e0df8adbb
minh-p <minhisverynormal@gmail.com>
2025-05-17 09:46:11 -0700

    add: Cmake is back; basic window pop up

--- Platformer/default.nix
+++ Platformer/default.nix
@@ -1,12 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.stdenv.mkDerivation {
   name = "Platformer";
-  src = ./src;
-  buildInputs = [ pkgs.gcc pkgs.sdl3 ];
-  buildPhase = "c++ -o Platformer main.cpp -lSDL3";
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp Platformer $out/bin/
-  '';
+  src = ./.;
+  nativeBuildInputs = [ pkgs.cmake pkgs.pkg-config ];
+  buildInputs = [ pkgs.sdl3 ];
 }
--- Platformer/src/main.cpp
+++ Platformer/src/main.cpp
@@ -1,45 +1,84 @@
+/*
+** Following Hamdy Elzanqali's tutorial.
+*/
+
+#include "SDL3/SDL_events.h"
+#include "SDL3/SDL_gpu.h"
+#include "SDL3/SDL_init.h"
+#include "SDL3/SDL_video.h"
+#define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL.h>
-#include <stdio.h>
+#include <SDL3/SDL_main.h>
 
-int main(int arc, char **argv) {
-  SDL_Window *window; // Declare a pointer
-  bool done = false;
+SDL_Window *window;
+SDL_GPUDevice *device;
 
-  SDL_Init(SDL_INIT_VIDEO); // Initialize SDL3
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+  window = SDL_CreateWindow("Hello, Triangle", 960, 540, SDL_WINDOW_RESIZABLE);
 
-  // Create an application window with the following settings:
-  printf("Creating window\n");
-  window = SDL_CreateWindow("An SDL3 window",    // window title
-                            640,                 // width, in pixels
-                            480,                 // height, in pixels
-                            SDL_WINDOW_RESIZABLE // flags - see below
-  );
-  printf("Window created\n");
+  device = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV, false, NULL);
+  SDL_ClaimWindowForGPUDevice(device, window);
 
-  // Check that the window was successfully created
-  if (window == NULL) {
-    // In the case that the window could not be made...
-    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n",
-                 SDL_GetError());
-    return 1;
+  return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+  SDL_GPUCommandBuffer *commandBuffer = SDL_AcquireGPUCommandBuffer(device);
+
+  SDL_GPUTexture *swapchainTexture;
+  Uint32 width, height;
+  // The command buffer is the storage for all the commands, while the swapchain
+  // is also a buffer.
+  // A swap chain is a collection of buffers that are used for displaying frames
+  // to the user.
+  SDL_WaitAndAcquireGPUSwapchainTexture(commandBuffer, window,
+                                        &swapchainTexture, &width, &height);
+
+  if (swapchainTexture == NULL) {
+    // end the frame early if a swapchain texture is not available
+    // SDL_SubmitGPUCommandBuffer has to be called at the end of every frame.
+    SDL_SubmitGPUCommandBuffer(commandBuffer);
+    return SDL_APP_CONTINUE;
   }
 
-  while (!done) {
-    SDL_Event event;
+  // The argument colorTargetInfo tells the GPU where to draw, what to do with
+  // previous target's content, and how to deal with new data.
 
-    while (SDL_PollEvent(&event)) {
-      if (event.type == SDL_EVENT_QUIT) {
-        done = true;
-      }
-    }
+  // create the color target
+  SDL_GPUColorTargetInfo colorTargetInfo{};
+  // Still learning C. 255.0f is basically 255 in float. These arguments will be
+  // from 0 to 1.
+  colorTargetInfo.clear_color = {240 / 255.0f, 240 / 255.0f, 240 / 255.0f,
+                                 255 / 255.0f};
+  // What to do with previous target's content.
+  colorTargetInfo.load_op = SDL_GPU_LOADOP_CLEAR;
+  // We're going to store the content to the texture.
+  colorTargetInfo.store_op = SDL_GPU_STOREOP_STORE;
+  // The texture aforementioned.
+  colorTargetInfo.texture = swapchainTexture;
 
-    // Do game logic, present a frame, etc.
+  // begin a render pass
+  SDL_GPURenderPass *renderPass =
+      SDL_BeginGPURenderPass(commandBuffer, &colorTargetInfo, 1, NULL);
+
+  // draw something
+
+  // end the render pass
+  SDL_EndGPURenderPass(renderPass);
+
+  // submit the command buffer
+  SDL_SubmitGPUCommandBuffer(commandBuffer);
+  return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+  if (event->type == SDL_EVENT_WINDOW_CLOSE_REQUESTED) {
+    return SDL_APP_SUCCESS;
   }
+  return SDL_APP_CONTINUE;
+}
 
-  // Close and destroy the window
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+  SDL_DestroyGPUDevice(device);
   SDL_DestroyWindow(window);
-
-  // Clean up
-  SDL_Quit();
-  return 0;
 }
