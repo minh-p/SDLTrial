cf751aa935a101774e95e9d5177adbabe3f1927b
minh-p <minhisverynormal@gmail.com>
2025-05-17 13:14:04 -0700

    add: finished Triangle. shaders relative path
    This is a note of concern. I want the program to be ran anywhere.
    There's more to do with the shaders path.

--- Triangle/default.nix
+++ Triangle/default.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.stdenv.mkDerivation {
-  name = "Platformer";
+  name = "Triangle";
   src = ./.;
   nativeBuildInputs = [ pkgs.cmake ];
   buildInputs = [
--- Triangle/src/main.cpp
+++ Triangle/src/main.cpp
@@ -36,6 +36,7 @@ SDL_Window *window;
 SDL_GPUDevice *device;
 SDL_GPUBuffer *vertexBuffer;
 SDL_GPUTransferBuffer *transferBuffer;
+SDL_GPUGraphicsPipeline *graphicsPipeline;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
   window = SDL_CreateWindow("Hello, Triangle", 960, 540, SDL_WINDOW_RESIZABLE);
@@ -76,6 +77,127 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
   // Unmapping as we're donig updating the transfer buffer
   SDL_UnmapGPUTransferBuffer(device, transferBuffer);
 
+  // Shaders
+  size_t vertexCodeSize;
+  void *vertexCode = SDL_LoadFile("shaders/vertex.vert.spv", &vertexCodeSize);
+  if (!vertexCode) {
+    SDL_Log("Failed to load vertex shader.");
+    return SDL_APP_FAILURE;
+  }
+
+  // Vertex Shader
+  SDL_GPUShaderCreateInfo vertexInfo{};
+  vertexInfo.code = (Uint8 *)vertexCode; // Converting to an array of bytes
+  vertexInfo.code_size = vertexCodeSize;
+  // entrypoint is the name of the function defined in the shader.
+  vertexInfo.entrypoint = "main";
+  vertexInfo.format = SDL_GPU_SHADERFORMAT_SPIRV;
+  vertexInfo.stage = SDL_GPU_SHADERSTAGE_VERTEX;
+  vertexInfo.num_samplers = 0;
+  vertexInfo.num_storage_buffers = 0;
+  vertexInfo.num_storage_textures = 0;
+  vertexInfo.num_uniform_buffers = 0;
+  SDL_GPUShader *vertexShader = SDL_CreateGPUShader(device, &vertexInfo);
+
+  // Then free the shader file
+  SDL_free(vertexCode);
+
+  // create the fragment shader
+  size_t fragmentCodeSize;
+  void *fragmentCode =
+      SDL_LoadFile("shaders/fragment.frag.spv", &fragmentCodeSize);
+
+  if (!fragmentCode) {
+    SDL_Log("Failed to load fragment shader.");
+    return SDL_APP_FAILURE;
+  }
+
+  // create the fragment shader
+  SDL_GPUShaderCreateInfo fragmentInfo{};
+  fragmentInfo.code = (Uint8 *)fragmentCode;
+  fragmentInfo.code_size = fragmentCodeSize;
+  fragmentInfo.entrypoint = "main";
+  fragmentInfo.format = SDL_GPU_SHADERFORMAT_SPIRV;
+  fragmentInfo.stage = SDL_GPU_SHADERSTAGE_FRAGMENT; // fragment shader
+  fragmentInfo.num_samplers = 0;
+  fragmentInfo.num_storage_buffers = 0;
+  fragmentInfo.num_storage_textures = 0;
+  fragmentInfo.num_uniform_buffers = 0;
+
+  SDL_GPUShader *fragmentShader = SDL_CreateGPUShader(device, &fragmentInfo);
+
+  // free the file
+  SDL_free(fragmentCode);
+
+  // Graphics Pipeline
+  SDL_GPUGraphicsPipelineCreateInfo pipelineInfo{};
+
+  // bind shaders
+  pipelineInfo.vertex_shader = vertexShader;
+  pipelineInfo.fragment_shader = fragmentShader;
+
+  // draw triangles
+  // Primitive types tell the GPU what vertices mean. Right now, we're drawing
+  // triangles.
+  // Try changing it to draw points or lines later.
+  pipelineInfo.primitive_type = SDL_GPU_PRIMITIVETYPE_TRIANGLELIST;
+
+  // Buffers are we expecting
+
+  SDL_GPUVertexBufferDescription vertexBufferDescriptions[1];
+  // assigned slot 0 will be used later when binding the buffer just before the
+  // draw call.
+  vertexBufferDescriptions[0].slot = 0;
+  // input rate set to be per vertex and not per instance.
+  // select data from the buffer will be changed to the next on every vertex.
+  // e.g. first vertex gets vertices[0], second get vertices[0].
+  vertexBufferDescriptions[0].input_rate = SDL_GPU_VERTEXINPUTRATE_VERTEX;
+  vertexBufferDescriptions[0].instance_step_rate = 0;
+  // pitch is how many bytes to jump after each cycle.
+  vertexBufferDescriptions[0].pitch = sizeof(Vertex);
+
+  pipelineInfo.vertex_input_state.num_vertex_buffers = 1;
+  pipelineInfo.vertex_input_state.vertex_buffer_descriptions =
+      vertexBufferDescriptions;
+
+  // Describing the vertex attribute
+
+  SDL_GPUVertexAttribute vertexAttributes[2];
+
+  // a_position
+  vertexAttributes[0].buffer_slot = 0; // fetch data from the buffer at slot 0
+  vertexAttributes[0].location = 0;    // layout (location = 0) in shader
+  vertexAttributes[0].format = SDL_GPU_VERTEXELEMENTFORMAT_FLOAT3; // vec3
+  vertexAttributes[0].offset =
+      0; // start from the first byte from current buffer position
+
+  // a_color
+  vertexAttributes[1].buffer_slot = 0; // use buffer at slot 0
+  vertexAttributes[1].location = 1;    // layout (location = 1) in shader
+  vertexAttributes[1].format = SDL_GPU_VERTEXELEMENTFORMAT_FLOAT4; // vec4
+  vertexAttributes[1].offset =
+      sizeof(float) * 3; // 4th float from current buffer position
+
+  pipelineInfo.vertex_input_state.num_vertex_attributes = 2;
+  pipelineInfo.vertex_input_state.vertex_attributes = vertexAttributes;
+
+  // Now, what's the color target that this pipeline is created to work on?
+  // describe the color target
+  SDL_GPUColorTargetDescription colorTargetDescriptions[1];
+  colorTargetDescriptions[0] = {};
+  colorTargetDescriptions[0].format =
+      SDL_GetGPUSwapchainTextureFormat(device, window);
+
+  pipelineInfo.target_info.num_color_targets = 1;
+  pipelineInfo.target_info.color_target_descriptions = colorTargetDescriptions;
+
+  // Officially creating the GPU graphics pipeline.
+  graphicsPipeline = SDL_CreateGPUGraphicsPipeline(device, &pipelineInfo);
+
+  // Releasing the shaders as we don't need the manymore.
+  SDL_ReleaseGPUShader(device, vertexShader);
+  SDL_ReleaseGPUShader(device, fragmentShader);
+
   // Updating the vertex buffer after creating the transfer buffer.
   // Transferring data from the transfer buffer to the vertex buffer is a
   // process known as a Copy pass.
@@ -101,28 +223,6 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
   region.size = sizeof(vertices);
   region.offset = 0;
 
-  // Shaders
-  size_t vertexCodeSize;
-  void *vertexCode = SDL_LoadFile("shaders/vertex.spv", &vertexCodeSize);
-
-  // Vertex Shader
-  SDL_GPUShaderCreateInfo vertexInfo{};
-  vertexInfo.code = (Uint8 *)vertexCode; // Converting to an array of bytes
-  vertexInfo.code_size = vertexCodeSize;
-  // entrypoint is the name of the function defined in the shader.
-  vertexInfo.entrypoint = "main";
-  vertexInfo.format = SDL_GPU_SHADERFORMAT_SPIRV;
-  vertexInfo.stage = SDL_GPU_SHADERSTAGE_VERTEX;
-  vertexInfo.num_samplers = 0;
-  vertexInfo.num_storage_buffers = 0;
-  vertexInfo.num_storage_textures = 0;
-  vertexInfo.num_uniform_buffers = 0;
-  SDL_GPUShader *vertexShader = SDL_CreateGPUShader(device, &vertexInfo);
-
-  // Then free the shader file
-  SDL_free(vertexCode);
-  SDL_ReleaseGPUShader(device, vertexShader);
-  //
   // uploading the data
   SDL_UploadToGPUBuffer(copyPass, &location, &region, true);
 
@@ -172,7 +272,22 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
   SDL_GPURenderPass *renderPass =
       SDL_BeginGPURenderPass(commandBuffer, &colorTargetInfo, 1, NULL);
 
-  // draw something
+  // Draw something
+
+  // First, we are binding the pipeline.
+  SDL_BindGPUGraphicsPipeline(renderPass, graphicsPipeline);
+
+  // Bind the vertex buffer
+  SDL_GPUBufferBinding bufferBindings[1];
+  // Know that index 0 is slot 0.
+  bufferBindings[0].buffer = vertexBuffer;
+  bufferBindings[0].offset = 0;
+
+  // Binding one buffer starting from slot 0.
+  SDL_BindGPUVertexBuffers(renderPass, 0, bufferBindings, 1);
+
+  // Finally, we're drawing the triangle!
+  SDL_DrawGPUPrimitives(renderPass, 3, 1, 0, 0);
 
   // end the render pass
   SDL_EndGPURenderPass(renderPass);
@@ -192,6 +307,7 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
   SDL_ReleaseGPUBuffer(device, vertexBuffer);
   SDL_ReleaseGPUTransferBuffer(device, transferBuffer);
+  SDL_ReleaseGPUGraphicsPipeline(device, graphicsPipeline);
   SDL_DestroyGPUDevice(device);
   SDL_DestroyWindow(window);
 }
